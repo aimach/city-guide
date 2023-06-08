fe7336ab7860d3ca04a44b26537973adc2fc5e1b
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-06-08 10:52:04 +0200

    Create endpoints

---
+++ backend/src/routes/profile-routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+export const profileRoutes = express.Router();
+
+profileRoutes.get(
+   '/:id'
+   // profileController.getOneProfile
+);
+
+profileRoutes.put(
+   '/:id'
+   // profileController.updateProfile
+);
+
+// Route et controller pour supprimer un profil en cas de désinscription ?
