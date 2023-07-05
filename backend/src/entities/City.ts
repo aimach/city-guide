7d88fde889a6232d992479ebcf6bffd43cba55d6
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-07-05 10:31:48 +0200

    S03_controllers : remove models and add entities

---
+++ backend/src/entities/City.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  Point,
+  OneToMany,
+  ManyToMany,
+  OneToOne,
+  JoinColumn,
+  Entity,
+} from "typeorm";
+import { Poi } from "./Poi";
+import { User } from "./User";
+
+@Entity()
+export class City {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", length: 100 })
+  name: string;
+
+  @Column("point")
+  coordinates: Point;
+
+  // ajout du type text
+  @Column("text")
+  image: string;
+
+  @OneToMany(() => Poi, (poi) => poi.city_id)
+  poi: Poi[];
+
+  @ManyToMany(() => User, (user) => user.favouriteCities)
+  users: User[];
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user_admin_city: User;
+}
