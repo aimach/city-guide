ac92f0b28b1707b7280167bbaec89ec689c611f8
celine.renault <renault.celine@icloud.com>
2023-07-06 17:28:51 +0200

    fixed eslint error and added info to Readme

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express';
-import * as dotenv from 'dotenv';
-import { User } from '../entities/User';
-import dataSource from '../dataSource';
-import * as bcrypt from 'bcrypt';
-import { sign } from 'jsonwebtoken';
-import { hash } from 'bcrypt';
-import validator from 'validator';
+import { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import { User } from "../entities/User";
+import dataSource from "../dataSource";
+import * as bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { hash } from "bcrypt";
+import validator from "validator";
 
 dotenv.config();
 
@@ -14,117 +14,114 @@ const TOKEN = process.env.TOKEN;
 // REGISTER
 
 interface IController {
-   [key: string]: (arg0: Request, arg1: Response) => {};
+	[key: string]: (arg0: Request, arg1: Response) => {};
 }
 
 export const AuthController: IController = {
-   register: async (req: Request, res: Response) => {
-      const { username, email, password } = req.body;
+	register: async (req: Request, res: Response) => {
+		const { username, email, password } = req.body;
 
-      const checkIfEmpty = (value: string) => {
-         if (validator.isEmpty(value, { ignore_whitespace: true })) {
-            return res
-               .status(401)
-               .send({ error: `Please fill the empty field` });
-         }
-      };
+		const checkIfEmpty = (value: string): void => {
+			if (validator.isEmpty(value, { ignore_whitespace: true })) {
+				res.status(401).send({ error: `Please fill the empty field` });
+			}
+		};
 
-      try {
-         let userToCreate = new User();
-         userToCreate = { ...userToCreate, email, password, username };
+		try {
+			let userToCreate = new User();
+			userToCreate = { ...userToCreate, email, password, username };
 
-         // Check if one of fields is empty
-         const inputs: string[] = Object.values(req.body);
-         inputs.forEach((value) => checkIfEmpty(value));
+			// Check if one of fields is empty
+			const inputs: string[] = Object.values(req.body);
+			inputs.forEach((value) => checkIfEmpty(value));
 
-         const existingEmail = await dataSource
-            .getRepository(User)
-            .findOneBy({ email });
+			const existingEmail = await dataSource
+				.getRepository(User)
+				.findOneBy({ email });
 
-         // Check if email alrealy exists
+			// Check if email alrealy exists
 
-         if (existingEmail) {
-            return res.status(409).send({ error: 'Email already exists' });
-         }
+			if (existingEmail !== null) {
+				return res.status(409).send({ error: "Email already exists" });
+			}
 
-         // Check if username is already taken
+			// Check if username is already taken
 
-         const existingUsername = await dataSource
-            .getRepository(User)
-            .findOneBy({ username });
-         if (existingUsername) {
-            return res.status(409).send({
-               error: 'This username is taken, please choose another one',
-            });
-         }
+			const existingUsername = await dataSource
+				.getRepository(User)
+				.findOneBy({ username });
+			if (existingUsername !== null) {
+				return res.status(409).send({
+					error: "This username is taken, please choose another one",
+				});
+			}
 
-         if (validator.isEmail(email) === false) {
-            return res.status(401).send({ error: 'Incorrect email format' });
-         }
+			if (!validator.isEmail(email)) {
+				return res.status(401).send({ error: "Incorrect email format" });
+			}
 
-         if (
-            validator.isStrongPassword(password, {
-               minLength: 8,
-               minNumbers: 1,
-               minUppercase: 1,
-               minSymbols: 1,
-            }) === false
-         ) {
-            return res.status(401).send({
-               error: 'Password must contain 8 characters, 1 digit, 1 uppercase and 1 symbol',
-            });
-         }
-         const hashedPassword = await hash(password, 10);
-         const newUser = { ...userToCreate, password: hashedPassword };
-         await dataSource.getRepository(User).save(newUser);
-         const token = sign(
-            { userId: newUser.id, role: newUser.role },
-            TOKEN as string,
-            {
-               expiresIn: '1h',
-            }
-         );
-         // envoi token dans le cookie
-         res.cookie('token', token, { httpOnly: true });
-         return res.status(201).send(token);
-      } catch (error) {
-         console.log(error);
-      }
-   },
+			if (
+				!validator.isStrongPassword(password, {
+					minLength: 8,
+					minNumbers: 1,
+					minUppercase: 1,
+					minSymbols: 1,
+				})
+			) {
+				return res.status(401).send({
+					error:
+						"Password must contain 8 characters, 1 digit, 1 uppercase and 1 symbol",
+				});
+			}
+			const hashedPassword = await hash(password, 10);
+			const newUser = { ...userToCreate, password: hashedPassword };
+			await dataSource.getRepository(User).save(newUser);
+			const token = sign(
+				{ userId: newUser.id, role: newUser.role },
+				TOKEN as string,
+				{
+					expiresIn: "1h",
+				}
+			);
+			// send token to cookie
+			res.cookie("token", token, { httpOnly: true });
+			return res.status(201).send(token);
+		} catch (error) {
+			console.log(error);
+		}
+	},
 
-   // LOGIN
+	// LOGIN
 
-   login: async (req: Request, res: Response) => {
-      const { email, password } = req.body;
+	login: async (req: Request, res: Response) => {
+		const { email, password } = req.body;
 
-      // recuperation de l'email de l'utilisateur
-      const getUserByEmail = await dataSource
-         .getRepository(User)
-         .findOneBy({ email });
+		// retrieve user email
+		const getUserByEmail = await dataSource
+			.getRepository(User)
+			.findOneBy({ email });
 
-      // verificaiton si je n'ai pas d'email
-      if (getUserByEmail === null) {
-         return res.status(404).send({ error: 'Invalid credentials' });
-      }
+		// check if I don't have email
+		if (getUserByEmail === null) {
+			return res.status(404).send({ error: "Invalid credentials" });
+		}
 
-      const user = getUserByEmail;
-      const validPassword = await bcrypt.compare(password, user.password);
+		const user = getUserByEmail;
+		const validPassword = await bcrypt.compare(password, user.password);
 
-      //verification si je n'ai pas le bon mot de passe
-      if (!validPassword) {
-         return res.status(400).send({ error: 'Invalid credentials' });
-      } else {
-         const token = sign(
-            { userId: getUserByEmail.id, role: getUserByEmail.role },
-            TOKEN as string,
-            {
-               expiresIn: '1h',
-            }
-         );
-         res.cookie('token', token, { httpOnly: true });
-         return res.status(200).send(token);
-      }
-   },
+		// check if I don't have the right password
+		if (!validPassword) {
+			return res.status(400).send({ error: "Invalid credentials" });
+		} else {
+			const token = sign(
+				{ userId: getUserByEmail.id, role: getUserByEmail.role },
+				TOKEN as string,
+				{
+					expiresIn: "1h",
+				}
+			);
+			res.cookie("token", token, { httpOnly: true });
+			return res.status(200).send(token);
+		}
+	},
 };
-
-// Voir cookie-parser pour middleware
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import dataSource from './dataSource';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import dataSource from "./dataSource";
+import cookieParser from "cookie-parser";
 import {
-   authRoutes,
-   poiRoutes,
-   profileRoutes,
-   citiesRoutes,
-   categoriesRoutes,
-} from './routes';
+	authRoutes,
+	poiRoutes,
+	profileRoutes,
+	citiesRoutes,
+	categoriesRoutes,
+} from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/poi', poiRoutes);
-app.use('/api/cities', citiesRoutes);
-app.use('/api/categories', categoriesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/poi", poiRoutes);
+app.use("/api/cities", citiesRoutes);
+app.use("/api/categories", categoriesRoutes);
 
 const start = async (): Promise<void> => {
-   const port = 5000;
+	const port = 5000;
 
-   await dataSource.initialize();
-   app.listen({ port }, () => {
-      console.log(`Backend app ready at http://localhost:${port}`);
-   });
+	await dataSource.initialize();
+	app.listen({ port }, () => {
+		console.log(`Backend app ready at http://localhost:${port}`);
+	});
 };
 void start();
