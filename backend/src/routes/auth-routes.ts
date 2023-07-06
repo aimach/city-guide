6cccc040673d42e532a206651ac2f55edab043eb
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-07-06 16:17:30 +0200

    New rate limit middleware

--- backend/src/routes/auth-routes.ts
+++ backend/src/routes/auth-routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { AuthController } from '../controllers/user-controller';
+import { limiter } from '../middlewares/limiter';
 
 export const authRoutes = express.Router();
 
-authRoutes.post('/login', AuthController.login);
+authRoutes.post('/login', limiter, AuthController.login);
 
-authRoutes.post('/register', AuthController.register);
+authRoutes.post('/register', limiter, AuthController.register);
