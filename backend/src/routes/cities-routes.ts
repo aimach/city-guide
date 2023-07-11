9db48ceefb1e6458c1cbcf818947af9cad34c3d9
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-07-11 15:46:32 +0200

    Add data and role validation to profile and city controllers

--- backend/src/routes/cities-routes.ts
+++ backend/src/routes/cities-routes.ts
@@ -1,24 +1,24 @@
-import express from "express";
-import multer from "multer";
+import express from 'express';
+import multer from 'multer';
 
-import { auth } from "../middlewares/auth";
-import { CityController } from "../controllers/cityController";
+import { auth } from '../middlewares/auth';
+import { CityController } from '../controllers/cityController';
 
-const upload = multer({ dest: "./public/city" });
+const upload = multer({ dest: './public/city' });
 
 export const citiesRoutes = express.Router();
 
-citiesRoutes.get("/", CityController.getCities);
+citiesRoutes.get('/', CityController.getCities);
 
-citiesRoutes.get("/:id", CityController.getOneCity);
+citiesRoutes.get('/:id', CityController.getOneCity);
 
-citiesRoutes.post("/", auth, upload.single("image"), CityController.createCity);
+citiesRoutes.post('/', auth, upload.single('image'), CityController.createCity);
 
 citiesRoutes.put(
-  "/:id",
-  auth,
-  upload.single("image"),
-  CityController.updateCity
+   '/:id',
+   auth,
+   upload.single('image'),
+   CityController.updateCity
 );
 
-citiesRoutes.delete("/:id", auth, CityController.deleteCity);
+citiesRoutes.delete('/:id', auth, CityController.deleteCity);
