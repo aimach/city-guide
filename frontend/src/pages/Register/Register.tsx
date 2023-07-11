7924f526dcfb039d6d411d1d689367019aa7acd9
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-07-11 16:22:15 +0200

    Fix: cors error

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,70 +1,78 @@
-import { useForm } from "react-hook-form"
-import '../../style/form.module.scss'
-import { useNavigate } from "react-router-dom"
+import { useForm } from 'react-hook-form';
+import '../../style/form.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export interface FormProps {
-  email: string
-  password: string
-  username :string
+   email: string;
+   password: string;
+   username: string;
 }
 
 const Register = () => {
-  const {handleSubmit, register} = useForm<FormProps>();
+   const { handleSubmit, register } = useForm<FormProps>();
 
-  const navigate = useNavigate();
+   const navigate = useNavigate();
 
-  const onSubmit = async (userData: FormProps) => {
-    try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
-        method: "POST",
-        credentials: "same-origin",
-        headers: { 
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData) 
-      })
-      
-      const data = await response.json()
-      if (data.token) {
+   const onSubmit = async (userData: FormProps) => {
+      try {
+         const response = await fetch(
+            'http://localhost:5000/api/auth/register',
+            {
+               method: 'POST',
+               credentials: 'include',
+               headers: {
+                  'Content-Type': 'application/json',
+                  Accept: 'application/json',
+               },
+               body: JSON.stringify(userData),
+            }
+         );
+
+         const data = await response.json();
+         console.log(data);
+         /* if (data.token) {
         localStorage.setItem("jwt_autorization", data.token)
         navigate('/')
         return
+      } */
+
+         // Handle other cases here
+      } catch (error) {
+         console.error(error);
       }
+   };
 
-      // Handle other cases here
+   return (
+      <section>
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <h3>Inscription</h3>
+            <input
+               type="text"
+               placeholder="Adresse mail"
+               {...register('email', {
+                  required: 'Vous devez renseigner ce champ',
+               })}
+            />
+            <input
+               type="text"
+               placeholder="Nom d'utilisateur"
+               {...register('username', {
+                  required: 'Vous devez renseigner ce champ',
+               })}
+            />
 
-    } catch (error) {
-      console.error(error);
-    }
-  }
+            <input
+               type="password"
+               placeholder="Mot de passe"
+               {...register('password', {
+                  required: 'Vous devez renseigner ce champ',
+               })}
+            />
 
+            <input type="submit" value="Explorer" />
+         </form>
+      </section>
+   );
+};
 
-  return (
-    <section>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h3>Inscription</h3>
-      <input type="text" placeholder='Adresse mail' 
-      {...register("email", {
-        required: "Vous devez renseigner ce champ"
-      })}
-      />
-      <input type="text" placeholder="Nom d'utilisateur" 
-      {...register("username", {
-        required: "Vous devez renseigner ce champ"
-      })}
-      />
-
-          <input type="password" placeholder='Mot de passe'
-          {...register("password", {
-            required: "Vous devez renseigner ce champ"
-        })}
-        />
-
-    <input type="submit" value="Explorer"/>
-
-    </form>
-    </section>
-  )
-}
-
-export default Register
+export default Register;
