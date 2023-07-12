5960e88df139bbf4dd23cbf1188183d3027081c1
celine.renault <renault.celine@icloud.com>
2023-07-12 12:06:57 +0200

    import image with wave and add titles

--- frontend/src/pages/SearchPOI/SearchPOI.tsx
+++ frontend/src/pages/SearchPOI/SearchPOI.tsx
@@ -1,8 +1,14 @@
 import Dropdown from "../../Components/filterSearch/dropdown/Dropdown";
+import styles from "./SearchPOI.module.scss";
+
 const SearchPOI = () => {
 	return (
-		<div>
+		<div className={`${styles.mainSearchPOI}`}>
 			<section>
+				<h2 className={`titlePOI`}>Trouvez votre point d’intéret !</h2>
+				<h3 className={`subtitlePOI`}>
+					Laissez vous portez ! Nous allons vous faire découvrir des merveilles
+				</h3>
 				<p>SearchPOI</p>
 				<Dropdown></Dropdown>
 			</section>
