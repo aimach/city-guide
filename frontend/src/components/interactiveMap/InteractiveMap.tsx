0104ce76d74d496e9504aa9de6e583de42888c4d
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-07-13 14:42:14 +0200

    feature: la carte "j'ai ajouté la géolocalisation"

--- frontend/src/components/interactiveMap/InteractiveMap.tsx
+++ frontend/src/components/interactiveMap/InteractiveMap.tsx
@@ -1,65 +1,69 @@
 import React, { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
+import maplibregl, { GeolocateControl } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import styles from "./InteractiveMap.module.scss";
-import { GeocodingControl } from "@maptiler/geocoding-control/maplibregl";
+import { createMapLibreGlMapController } from "@maptiler/geocoding-control/maplibregl";
+import { GeocodingControl } from "@maptiler/geocoding-control/react";
 
 export default function InteractiveMap() {
-  const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<maplibregl.Map>();
-  const marker = useRef<maplibregl.Marker>();
+  const mapContainer = useRef<any>(null);
+  const map = useRef<any>(null);
   const [lng] = useState(139.753);
   const [lat] = useState(35.6844);
   const [zoom] = useState(14);
   const [API_KEY] = useState("bUNkGxzojTZOBuCGooQ2");
+  const [mapController, setMapController] = useState<any>();
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
 
   useEffect(() => {
-    if (map.current) return;
+    if (map.current) return; // stops map from intializing more than once
 
     map.current = new maplibregl.Map({
-      container: mapContainer.current!,
-      style: `https://api.maptiler.com/maps/satellite/style.json?key=${API_KEY}`,
+      container: mapContainer.current,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
       center: [lng, lat],
       zoom: zoom,
     });
 
-    map.current.addControl(new maplibregl.NavigationControl(), "top-right");
-    new maplibregl.Marker({ color: "#FF0000" })
-      .setLngLat([139.7525, 35.6846])
-      .addTo(map.current);
-
-    marker.current = new maplibregl.Marker({ color: "#FF0000" })
-      .setLngLat([139.7525, 35.6846])
-      .addTo(map.current);
-
-    const popup = new maplibregl.Popup({ offset: 25 }).setText(
-      "Hello, this is a popup!"
-    );
-    marker.current.setPopup(popup);
-
-    const geocodingControl = new GeocodingControl({
-      apiKey: API_KEY,
-      // @ts-ignore
-      maplibregl,
+    const geolocateControl = new GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
     });
-    map.current.addControl(geocodingControl, "top-left");
+    map.current.addControl(geolocateControl);
+
+    map.current.addControl(new maplibregl.NavigationControl(), "top-right");
+    setMapController(createMapLibreGlMapController(map.current, maplibregl));
   }, [API_KEY, lng, lat, zoom]);
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      const marker = new maplibregl.Marker()
-        .setLngLat([longitude, latitude])
-        .addTo(map.current!);
-    },
-    (error) => {
-      console.log(error.message);
-    }
-  );
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (error) => {
+        console.log("Erreur de géolocalisation :", error.message);
+      }
+    );
+  } else {
+    console.log(
+      "La géolocalisation n'est pas prise en charge par ce navigateur."
+    );
+
+    console.log("Latitude :", latitude);
+    console.log("Longitude :", longitude);
+  }
 
   return (
     <div className={`${styles.mapWrap}`}>
+      <div className={styles.geocoding}>
+        <GeocodingControl apiKey={API_KEY} mapController={mapController} />
+      </div>
       <div ref={mapContainer} className={`${styles.mapWrap}`} />
     </div>
   );
