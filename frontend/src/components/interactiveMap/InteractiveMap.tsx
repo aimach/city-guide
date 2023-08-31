6701e47e44f4475cb7f834aacd7c22a636c6e65a
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-08-31 12:14:14 +0200

    feat: la carte "creating a file to secure API-KEY for the map"

--- frontend/src/components/interactiveMap/InteractiveMap.tsx
+++ frontend/src/components/interactiveMap/InteractiveMap.tsx
@@ -11,7 +11,7 @@ export default function InteractiveMap() {
   const [lng] = useState(139.753);
   const [lat] = useState(35.6844);
   const [zoom] = useState(14);
-  const [API_KEY] = useState("bUNkGxzojTZOBuCGooQ2");
+
   const [mapController, setMapController] = useState<any>();
   const [latitude, setLatitude] = useState<number>(0);
   const [longitude, setLongitude] = useState<number>(0);
@@ -21,7 +21,7 @@ export default function InteractiveMap() {
 
     map.current = new maplibregl.Map({
       container: mapContainer.current,
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${process.env.REACT_APP_MAPTILER_API_KEY}`,
       center: [lng, lat],
       zoom: zoom,
     });
@@ -36,33 +36,36 @@ export default function InteractiveMap() {
 
     map.current.addControl(new maplibregl.NavigationControl(), "top-right");
     setMapController(createMapLibreGlMapController(map.current, maplibregl));
-  }, [API_KEY, lng, lat, zoom]);
 
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (error) => {
-        console.log("Erreur de géolocalisation :", error.message);
-      }
-    );
-  } else {
-    console.log(
-      "La géolocalisation n'est pas prise en charge par ce navigateur."
-    );
+          setLatitude(latitude);
+          setLongitude(longitude);
+        },
+        (error) => {
+          console.log("Erreur de géolocalisation :", error.message);
+        }
+      );
+    } else {
+      console.log(
+        "La géolocalisation n'est pas prise en charge par ce navigateur."
+      );
 
-    console.log("Latitude :", latitude);
-    console.log("Longitude :", longitude);
-  }
+      console.log("Latitude :", latitude);
+      console.log("Longitude :", longitude);
+    }
+  }, [lng, lat, zoom, latitude, longitude]);
 
   return (
     <div className={`${styles.mapWrap}`}>
       <div className={styles.geocoding}>
-        <GeocodingControl apiKey={API_KEY} mapController={mapController} />
+        <GeocodingControl
+          apiKey={process.env.REACT_APP_MAPTILER_API_KEY!}
+          mapController={mapController}
+        />
       </div>
       <div ref={mapContainer} className={`${styles.mapWrap}`} />
     </div>
