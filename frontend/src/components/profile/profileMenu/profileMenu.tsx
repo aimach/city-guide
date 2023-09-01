ef911e0d9ea768eb40b5afa9048ab417b1c502f7
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-09-01 12:20:42 +0200

    S05_04_profileMenu : add structure and style to component

--- frontend/src/components/profile/profileMenu/profileMenu.tsx
+++ frontend/src/components/profile/profileMenu/profileMenu.tsx
@@ -6,13 +6,16 @@ const ProfileMenu = () => {
   const { profile } = useContext(UsersContext);
 
   return (
-    <div className={`${style.avatarButton}`}>
-      {profile?.image !== null ? (
-        <img src={profile?.image} alt="avatar" />
-      ) : (
-        <p>{profile?.username.substring(0, 1).toUpperCase()}</p>
-      )}
-    </div>
+    <>
+      <button className={`${style.avatarButton}`}>
+        {profile?.image !== null ? (
+          <img src={profile?.image} alt="avatar" />
+        ) : (
+          <p>{profile?.username.substring(0, 1).toUpperCase()}</p>
+        )}
+      </button>
+      <div className={`${style.floatingMenu}`}> Menu de l'utilisateur</div>
+    </>
   );
 };
 
