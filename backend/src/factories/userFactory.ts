471062803661f789a57082e9cf58cc8b82cc0820
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-11-08 09:53:38 +0100

    add bio field to database in user table

--- backend/src/factories/userFactory.ts
+++ backend/src/factories/userFactory.ts
@@ -1,14 +1,15 @@
-import { Faker } from '@faker-js/faker';
-import { setSeederFactory } from 'typeorm-extension';
-import { User } from '../entities/User';
+import { Faker } from "@faker-js/faker";
+import { setSeederFactory } from "typeorm-extension";
+import { User } from "../entities/User";
 
 export const UsersFactory = setSeederFactory(User, (faker: Faker) => {
-   const user = new User();
-   user.username = faker.internet.userName();
-   user.email = faker.internet.email();
-   user.password = faker.internet.password();
-   user.image = faker.internet.avatar();
-   user.city = faker.location.city();
+  const user = new User();
+  user.username = faker.internet.userName();
+  user.email = faker.internet.email();
+  user.password = faker.internet.password();
+  user.bio = faker.lorem.paragraph();
+  user.image = faker.internet.avatar();
+  user.city = faker.location.city();
 
-   return user;
+  return user;
 });
