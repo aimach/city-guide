ab5e86095b71add7a601c86ba3075cd1e25ea27c
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-11-10 17:36:34 +0100

    feat: page faq "modificaton du styles et ajouts des icône plus et moins"

--- frontend/src/pages/Faq/FaqPage.tsx
+++ frontend/src/pages/Faq/FaqPage.tsx
@@ -1,46 +1,78 @@
-import { FaqItem, faqItems } from "../../constants/faq";
+import React, { useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+import { faqItems, FaqItem } from "../../constants/faq";
 import styles from "./faqPage.module.scss";
 
-const groupBySection = (items: FaqItem[]) => {
-  return items.reduce((acc, item) => {
-    acc[item.section] = acc[item.section] || [];
-    acc[item.section].push(item);
-    return acc;
-  }, {} as Record<string, FaqItem[]>);
-};
-
-const capitalizeFirstLetter = (string: any) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+type OpenDetails = Record<string, boolean>;
 
 const FaqPage = () => {
-  const groupedFaqItems = groupBySection(faqItems);
-  const sectionKeys = Object.keys(groupedFaqItems);
+  const [openDetails, setOpenDetails] = useState<OpenDetails>({});
+
+  const toggleOpen = (key: string) => {
+    setOpenDetails((prevOpenDetails: OpenDetails) => ({
+      ...prevOpenDetails,
+      [key]: !prevOpenDetails[key],
+    }));
+  };
+
+  const handleKeyDown = (
+    event: React.KeyboardEvent<HTMLElement>,
+    key: string
+  ) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      toggleOpen(key);
+    }
+  };
+
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  const faqSections = faqItems.reduce((acc, item, index) => {
+    const section = capitalizeFirstLetter(item.section);
+    const key = `${section}-${index}`;
+    if (!acc[section]) {
+      acc[section] = [];
+    }
+    acc[section].push({ ...item, key });
+    return acc;
+  }, {} as Record<string, (FaqItem & { key: string })[]>);
+
+  const addSpacesToCamelCase = (string: string) => {
+    return string
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (str) => str.toUpperCase());
+  };
 
   return (
     <div className={styles.faqPage}>
       <h1>FAQ</h1>
-      {sectionKeys.map((sectionKey) => {
-        // Capitalise la première lettre de chaque mot dans la section
-        const sectionDisplay = sectionKey
-          .split(/(?=[A-Z])/)
-          .map(capitalizeFirstLetter)
-          .join(" ");
-
-        return (
-          <section key={sectionKey} id={sectionKey}>
-            <div className={styles.faqHeader}>{sectionDisplay}</div>
-            <div className={styles.faqBody}>
-              {groupedFaqItems[sectionKey].map((item, index) => (
-                <details key={index}>
-                  <summary>{item.question}</summary>
-                  <p>{item.answer}</p>
-                </details>
-              ))}
-            </div>
-          </section>
-        );
-      })}
+      {Object.entries(faqSections).map(([section, items], sectionIndex) => (
+        <div key={section}>
+          <h2 className={styles.sectionTitle}>
+            {addSpacesToCamelCase(section)}
+          </h2>
+          {items.map((item, itemIndex) => {
+            const key = `${section}-${itemIndex}`;
+            const isOpen = openDetails[key];
+            return (
+              <details key={key} className={styles.faqItem}>
+                <summary
+                  onClick={() => toggleOpen(key)}
+                  onKeyDown={(event) => handleKeyDown(event, key)}
+                  tabIndex={0}
+                >
+                  {capitalizeFirstLetter(item.question)}
+                  <span className={styles.icon}>
+                    {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                  </span>
+                </summary>
+                {isOpen && <p>{item.answer}</p>}
+              </details>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 };
