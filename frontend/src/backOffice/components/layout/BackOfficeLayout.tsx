334e55827a27e11f30cacee39205307005b8bdf5
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-12-05 14:03:23 +0100

    feat: page faq "merge de la branche back-office Céline"

--- frontend/src/backOffice/components/layout/BackOfficeLayout.tsx
+++ frontend/src/backOffice/components/layout/BackOfficeLayout.tsx
@@ -5,37 +5,36 @@ import styles from "./BackOfficeLayout.module.scss";
 import { UsersContext } from "../../../contexts/UserContext";
 
 export default function BackOfficeLayout({ children }: PropsWithChildren) {
-	const [windowSize, setWindowSize] = useState<number>(window.innerWidth);
-	const { isAuthenticated, loaded, redirectToLogin } = useContext(UsersContext);
+  const [windowSize, setWindowSize] = useState<number>(window.innerWidth);
+  const { isAuthenticated, loaded, redirectToLogin } = useContext(UsersContext);
 
-	function updateDimension() {
-		setWindowSize(window.innerWidth);
-	}
-	useEffect(() => {
-		window.addEventListener("resize", updateDimension);
-	}, [windowSize]);
+  function updateDimension() {
+    setWindowSize(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", updateDimension);
+  }, [windowSize]);
 
-	useEffect(() => {
-		// Si on a fait la requête pour savoir si l'utilisateur est connecté
-		// et qu'il ne l'est pas,
-		// on le redirige vers la page de connexion.
-		if (!isAuthenticated() && loaded) {
-			redirectToLogin();
-		}
-	}, [isAuthenticated, loaded, redirectToLogin]);
+  useEffect(() => {
+    // Si on a fait la requête pour savoir si l'utilisateur est connecté
+    // et qu'il ne l'est pas,
+    // on le redirige vers la page de connexion.
+    if (!isAuthenticated() && loaded) {
+      redirectToLogin();
+    }
+  }, [isAuthenticated, loaded, redirectToLogin]);
 
-	return (
-		<>
-			<Header size={windowSize > 768 ? "desktop" : "mobile"} />
-			<div className={styles.layoutBackOffice}>
-				<section className={styles.flexAsideMenu}>
-					<AsideMenu />
-				</section>
-				<section className={styles.flexContent}>
-					<div className={styles.backOfficeContent}>{children}</div>
-					<div className={styles.footerWave}></div>
-				</section>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.layoutBackOffice}>
+        <section className={styles.flexAsideMenu}>
+          <AsideMenu />
+        </section>
+        <section className={styles.flexContent}>
+          <div className={styles.backOfficeContent}>{children}</div>
+          <div className={styles.footerWave}></div>
+        </section>
+      </div>
+    </>
+  );
 }
