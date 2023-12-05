7784bd80b5b1801cc8ade96d149b4989a5712126
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-12-05 10:40:06 +0100

    feat: page faq "ajout de l'image : fond_vague_creme.png"

--- frontend/src/pages/Faq/FaqPage.tsx
+++ frontend/src/pages/Faq/FaqPage.tsx
@@ -45,35 +45,38 @@ const FaqPage = () => {
   };
 
   return (
-    <div className={styles.faqPage}>
-      <h1>FAQ</h1>
-      {Object.entries(faqSections).map(([section, items], sectionIndex) => (
-        <div key={section}>
-          <h2 className={styles.sectionTitle}>
-            {addSpacesToCamelCase(section)}
-          </h2>
-          {items.map((item, itemIndex) => {
-            const key = `${section}-${itemIndex}`;
-            const isOpen = openDetails[key];
-            return (
-              <details key={key} className={styles.faqItem}>
-                <summary
-                  onClick={() => toggleOpen(key)}
-                  onKeyDown={(event) => handleKeyDown(event, key)}
-                  tabIndex={0}
-                >
-                  {capitalizeFirstLetter(item.question)}
-                  <span className={styles.icon}>
-                    {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
-                  </span>
-                </summary>
-                {isOpen && <p>{item.answer}</p>}
-              </details>
-            );
-          })}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className={styles.faqPage}>
+        <h1>FAQ</h1>
+        {Object.entries(faqSections).map(([section, items], sectionIndex) => (
+          <div key={section}>
+            <h2 className={styles.sectionTitle}>
+              {addSpacesToCamelCase(section)}
+            </h2>
+            {items.map((item, itemIndex) => {
+              const key = `${section}-${itemIndex}`;
+              const isOpen = openDetails[key];
+              return (
+                <details key={key} className={styles.faqItem}>
+                  <summary
+                    onClick={() => toggleOpen(key)}
+                    onKeyDown={(event) => handleKeyDown(event, key)}
+                    tabIndex={0}
+                  >
+                    {capitalizeFirstLetter(item.question)}
+                    <span className={styles.icon}>
+                      {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                    </span>
+                  </summary>
+                  {isOpen && <p>{item.answer}</p>}
+                </details>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+      <div className={styles.WaveImage}></div>
+    </>
   );
 };
 
