44f34b963f1413c2af5abd0871d64b540b3603ab
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-12-05 16:58:50 +0100

    add buttons to favorites page in mobile version

--- frontend/src/pages/Profile/ProfileFavorites.tsx
+++ frontend/src/pages/Profile/ProfileFavorites.tsx
@@ -1,5 +1,5 @@
 import style from "./profile.module.scss";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UsersContext } from "../../contexts/UserContext";
 import Caroussel from "../../components/common/Caroussel/Caroussel";
 import { CardType } from "../../utils/types";
@@ -7,6 +7,7 @@ import { IoIosHeart } from "react-icons/io";
 
 export default function ProfileFavorite() {
   const { profile } = useContext(UsersContext);
+  const [favType, setFavType] = useState("poi");
 
   return (
     <>
@@ -18,30 +19,47 @@ export default function ProfileFavorite() {
             strokeWidth={22}
           />
         </div>
-        <div>
-          <h4 className="profilNameDark">Points d'intérêts</h4>
-          {profile?.favouritePoi.length ? (
-            <Caroussel
-              title=""
-              data={profile?.favouritePoi}
-              cardType={CardType.POI}
-            />
-          ) : (
-            <p>Pas de favori pour l'instant !</p>
-          )}
+        <div className={style.buttonContainer}>
+          <button
+            type="button"
+            onClick={() => setFavType("poi")}
+            className={favType === "poi" ? style.buttonSelected : undefined}
+          >
+            Points d'intérêts
+          </button>
+          <button
+            type="button"
+            onClick={() => setFavType("city")}
+            className={favType === "city" ? style.buttonSelected : undefined}
+          >
+            Villes
+          </button>
         </div>
-        <div>
-          <h4 className="profilNameDark">Villes</h4>
-          {profile?.favouriteCities.length ? (
-            <Caroussel
-              title=""
-              data={profile?.favouriteCities}
-              cardType={CardType.CITY}
-            />
-          ) : (
-            <p>Pas de favori pour l'instant !</p>
-          )}
-        </div>
+        {favType === "poi" ? (
+          <div>
+            {profile?.favouritePoi.length ? (
+              <Caroussel
+                title=""
+                data={profile?.favouritePoi}
+                cardType={CardType.POI}
+              />
+            ) : (
+              <p>Pas de favori pour l'instant !</p>
+            )}
+          </div>
+        ) : (
+          <div>
+            {profile?.favouriteCities.length ? (
+              <Caroussel
+                title=""
+                data={profile?.favouriteCities}
+                cardType={CardType.CITY}
+              />
+            ) : (
+              <p>Pas de favori pour l'instant !</p>
+            )}
+          </div>
+        )}
       </section>
       <img src="/fond_vague_creme.svg" alt="wave" width="100%" />
     </>
