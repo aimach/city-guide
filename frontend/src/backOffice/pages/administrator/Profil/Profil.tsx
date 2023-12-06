50693e10692925014df8ccc8e1ec021245fa12f1
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-12-06 15:41:04 +0100

    feat: form back-office "mise à jour de la mise à forme du formulaire"

--- frontend/src/backOffice/pages/administrator/Profil/Profil.tsx
+++ frontend/src/backOffice/pages/administrator/Profil/Profil.tsx
@@ -4,47 +4,70 @@ import styles from "./Profil.module.scss";
 import Title from "../../../components/common/Title/Title";
 
 const Profil = () => {
+  const handleFileButtonClick = () => {
+    document.getElementById("imageAvatar")?.click();
+  };
   return (
     <BackOfficeLayout>
-      <Title name={"Profil"} icon={faUser}></Title>
-      <div>
+      <Title name={"Profil"} icon={faUser} />
+      <div className={styles.profilContainer}>
         <h4 className={styles.titleForm}>Modifier ses données du profil</h4>
-        <div className={styles.profilForm}>
-          <form>
-            <div className={styles.formGroup}>
+        <form className={styles.profilForm}>
+          <div className={styles.twoColumns}>
+            <div className={styles.inputGroup}>
               <label htmlFor="name">Nom</label>
-              <input type="text" />
+              <input id="name" type="text" name="name" placeholder="Nom" />
+            </div>
+            <div className={styles.inputGroup}>
               <label htmlFor="status">Statut</label>
-              <select name="status">
-                <option>Adminstrateur de ville</option>
+              <select id="status" name="status">
+                <option>Administrateur de ville</option>
                 <option>Utilisateur</option>
               </select>
             </div>
-            <div className={styles.formGroup}>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" />
-                <label htmlFor="password">Mot de passe</label>
-                <input type="password" />
-              </div>
+          </div>
+          <div className={styles.twoColumns}>
+            <div className={styles.inputGroup}>
+              <label htmlFor="email">Email</label>
+              <input id="email" type="email" name="email" placeholder="Email" />
             </div>
-            <div className={styles.formGroup}>
-              <label htmlFor="badge">Badge</label>
-              <input type="text" />
+            <div className={styles.inputGroup}>
+              <label htmlFor="password">Mot de passe</label>
+              <input
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Mot de passe"
+              />
             </div>
-            <div className={styles.fullWidth}>
-              <label htmlFor="imageAvatar">Image - Avatar</label>
-              <input type="file" name="imageAvatar" />
+          </div>
+          <div className={styles.fullWidth}>
+            <label htmlFor="imageAvatar">Image - Avatar</label>
+            <div className={styles.fileInputContainer}>
+              <input
+                id="imageAvatar"
+                type="file"
+                name="imageAvatar"
+                placeholder="Image"
+                style={{ display: "none" }}
+              />
+              <button type="button" onClick={handleFileButtonClick}>
+                ...
+              </button>
             </div>
-            <div className={styles.fullWidth}>
-              <label htmlFor="descriptionBio">Description - Bio</label>
-              <textarea name="descriptionBio" />
-            </div>
-            <div className={styles.fullWidth}>
-              <button type="submit">Ajouter</button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div className={styles.fullWidth}>
+            <label htmlFor="descriptionBio">Description - Bio</label>
+            <textarea
+              id="descriptionBio"
+              name="descriptionBio"
+              placeholder="Description"
+            />
+          </div>
+          <div className={styles.fullWidth}>
+            <button type="submit">Ajouter</button>
+          </div>
+        </form>
       </div>
     </BackOfficeLayout>
   );
