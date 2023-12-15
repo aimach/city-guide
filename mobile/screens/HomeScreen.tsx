77e413b63198cd960fc74bfbf9938bd85ceefcfc
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-12-15 11:53:16 +0100

    Add colors file and initiate card component

--- mobile/screens/HomeScreen.tsx
+++ mobile/screens/HomeScreen.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 export const HomeScreen = () => {
+   const getCities = async () => {
+      const response = await fetch('http://192.168.1.104:5000/api/cities');
+      const data = await response.json();
+      console.log(data);
+   };
+   getCities();
    return (
       <View>
          <Text>Home</Text>
---
+++ mobile/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+   primary_color: '#090f43',
+   secondary_color: '#f3df24',
+   cream_color: '#f5f6e2',
+   gray_color: '#d9d9d9',
+   gray_color_dark: '#6b6b6b',
+};
