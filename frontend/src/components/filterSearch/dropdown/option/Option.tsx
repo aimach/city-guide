35550bff9f06a8bd28fcca19a430aadb7ade5eb3
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-01-09 14:59:38 +0100

    remove useless components

--- frontend/src/components/filterSearch/dropdown/option/Option.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Option = () => {
-  return (
-    <div>Option</div>
-  )
-}
-
-export default Option
--- frontend/src/components/subscribe/CardSubscribe.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const cardSubscribe = () => {
-  return (
-    <div>cardSubscribe</div>
-  )
-}
-
-export default cardSubscribe
