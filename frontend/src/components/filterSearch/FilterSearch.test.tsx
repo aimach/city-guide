904d5ff29cefc3321b3d6970128e3e36723b08f3
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-01-09 10:28:15 +0100

    add test on input in filter search

--- frontend/src/components/filterSearch/FilterSearch.test.tsx
+++ frontend/src/components/filterSearch/FilterSearch.test.tsx
@@ -15,12 +15,18 @@ describe("FilterSearch", () => {
     );
   });
 
-  // it("should change value on change", () => {
-  //   render(
-  //     <FilterSearch
-  //       filterSearch={filterSearch}
-  //       setFilterSearch={setFilterSearch}
-  //     />
-  //   );
-  // });
+  it("should change value on change", () => {
+    render(
+      <FilterSearch
+        filterSearch={filterSearch}
+        setFilterSearch={setFilterSearch}
+      />
+    );
+
+    const input = screen.getByPlaceholderText(
+      /Rechercher une ville.../i
+    ) as HTMLInputElement;
+    fireEvent.change(input, { target: { value: "Test" } });
+    expect(setFilterSearch).toHaveBeenCalledTimes(1);
+  });
 });
