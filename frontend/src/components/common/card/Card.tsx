c76809d5967005e030495f8e46cfb49da9a8e54e
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-01-20 18:56:06 +0100

    add env var for backend url

--- frontend/src/components/common/card/Card.tsx
+++ frontend/src/components/common/card/Card.tsx
@@ -5,66 +5,66 @@ import { CardType, Category, City, Poi } from "../../../utils/types";
 import { UsersContext } from "../../../contexts/UserContext";
 
 interface Props {
-	data: Poi | Category | City;
-	cardType: CardType;
-	onClick: () => void;
-	categorySelected?: string | null;
-	currentCity?: City | null;
-	handleFavourite: (id: string | null) => void;
-	isLiked: (id: string | null) => boolean;
+  data: Poi | Category | City;
+  cardType: CardType;
+  onClick: () => void;
+  categorySelected?: string | null;
+  currentCity?: City | null;
+  handleFavourite: (id: string | null) => void;
+  isLiked: (id: string | null) => boolean;
 }
 
 const Card = ({
-	data,
-	cardType,
-	onClick,
-	categorySelected,
-	handleFavourite,
-	isLiked,
+  data,
+  cardType,
+  onClick,
+  categorySelected,
+  handleFavourite,
+  isLiked,
 }: Props) => {
-	const { isAuthenticated } = useContext(UsersContext);
+  const { isAuthenticated } = useContext(UsersContext);
 
-	const { id, name, image } = data;
-	const imageURL = image.includes("public")
-		? `http://localhost:5000${image}`
-		: image;
-	return (
-		<>
-			<div
-				className={`${styles.container} ${
-					categorySelected !== id &&
-					categorySelected != null &&
-					cardType === CardType.CATEGORY &&
-					styles.unselected
-				} `}
-			>
-				<div onClick={onClick} className={styles.imageContainer}>
-					<img src={imageURL} alt={name} className={styles.image} />
-					<h3 className={`${styles.title} titleCard`}>{name}</h3>
-				</div>
-				{cardType !== CardType.CATEGORY && isAuthenticated() ? (
-					<div
-						className={styles.likeContainer}
-						onClick={() => {
-							handleFavourite(id);
-						}}
-					>
-						{isLiked(id) ? (
-							<IoIosHeart
-								className={styles.filledHeart}
-								stroke="black"
-								strokeWidth={22}
-							/>
-						) : (
-							<IoIosHeartEmpty className={styles.emptyHeart} />
-						)}
-					</div>
-				) : (
-					""
-				)}
-			</div>
-		</>
-	);
+  const { id, name, image } = data;
+  const imageURL = image.includes("public")
+    ? `${process.env.REACT_APP_PUBLIC_BACKEND_URL}/${image}`
+    : image;
+  return (
+    <>
+      <div
+        className={`${styles.container} ${
+          categorySelected !== id &&
+          categorySelected != null &&
+          cardType === CardType.CATEGORY &&
+          styles.unselected
+        } `}
+      >
+        <div onClick={onClick} className={styles.imageContainer}>
+          <img src={imageURL} alt={name} className={styles.image} />
+          <h3 className={`${styles.title} titleCard`}>{name}</h3>
+        </div>
+        {cardType !== CardType.CATEGORY && isAuthenticated() ? (
+          <div
+            className={styles.likeContainer}
+            onClick={() => {
+              handleFavourite(id);
+            }}
+          >
+            {isLiked(id) ? (
+              <IoIosHeart
+                className={styles.filledHeart}
+                stroke="black"
+                strokeWidth={22}
+              />
+            ) : (
+              <IoIosHeartEmpty className={styles.emptyHeart} />
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Card;
--- frontend/src/pages/Contact/ContactPage.tsx
+++ frontend/src/pages/Contact/ContactPage.tsx
@@ -19,14 +19,17 @@ const ContactPage = () => {
     let response;
 
     try {
-      response = await fetch("http://localhost:5000/api/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify(messageData),
-      });
+      response = await fetch(
+        `${process.env.REACT_APP_PUBLIC_BACKEND_URL}/api/contact`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(messageData),
+        }
+      );
 
       if (response.ok) {
         await response.json();
