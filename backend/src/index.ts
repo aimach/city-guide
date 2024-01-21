86eddc169182f8a9a59966869ac5162a67b4ef47
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2024-01-21 13:11:08 +0100

    test for CD

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -61,7 +61,7 @@ const start = async (): Promise<void> => {
   await dataSource.initialize();
 
   app.listen({ port }, () => {
-    console.log(`Backend app ready on the server port ${port}`);
+    console.log(`Backend app ready at localhost:${port}`);
   });
 };
 void start();
